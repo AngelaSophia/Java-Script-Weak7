ced9bd7ae7cff10148b1d3806b76319cf3def842
AngelaSophia <ayugisophiaangela@gmail.com>
2021-04-05 09:28:13 +0300

    Happy

---
+++ Sophia.js
@@ -0,0 +1,45 @@
+function Human(){    
+var person ={fname:"Sophia",lname:"Ayugi",age:23};
+var text="";
+var x;
+for (x in person){
+    console.log(
+    text +=person[x]);
+}
+}Human();
+
+
+
+function vehicle(){
+var cars=["BMW","BENZ","TOYOTA"]
+for (i=0; i<cars.length; i++){
+    console.log(i);
+    
+    }
+}vehicle();
+
+function school(){
+    var AkiraChix ={class:"Lisa Lab",numberofstudent:72,streams:3}
+    var text ="";
+    var y;
+    for (y in AkiraChix){
+        console.log(
+          text +=AkiraChix [y]);
+    }  
+}school();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
